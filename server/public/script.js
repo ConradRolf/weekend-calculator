67d4dfb5215e299b41a23454d9073b3014c6a576
Conrad Rolf <williamconradrolf@gmail.com>
2023-05-14 12:52:02 -0500

    Commit your main branch before making a feature

--- server/public/script.js
+++ server/public/script.js
@@ -14,38 +14,6 @@ function onReady() {
 let operator;
 let numbersFunction;
 
-function dis(val) {
-    $('#result').value += val
-}
-
-function myFunction(event) {
-    if (event.key == '0' || event.key == '1' 
-        || event.key == '2' || event.key == '3'
-        || event.key == '4' || event.key == '5' 
-        || event.key == '6' || event.key == '7'
-        || event.key == '8' || event.key == '9' 
-        || event.key == '+' || event.key == '-'
-        || event.key == '*' || event.key == '/')
-        $('#result').value += event.key;
-}
-
-var cal = $('calcu');
-        cal.onkeyup = function (event) {
-            if (event.keyCode === 13) {
-                console.log("Enter");
-                let x = $('#result').val()
-                console.log(x);
-                solve();
-            }
-        }
-  
-        // Function that evaluates the digit and return result
-        function solve() {
-            let x = $('#result').val()
-            let y = eval(x)
-            $('#result').value = y
-        }
-
 function addNumbers(event){
     event.preventDefault();
 
@@ -96,7 +64,8 @@ function addNumbers(event){
 
 function clearInputs(event){
     event.preventDefault();
-    $('#result').val('')
+    $('#first-value').val('')
+    $('#second-value').val('')
 }
 
 // function getNumbers(){
@@ -163,7 +132,8 @@ function divide(event){
 
 
 function renderToDom(answer){
-    $('#result-list').append(numbersFunction, answer)
+    let result = answer
+    $('#result-list').append(numbersFunction, result)
 }
 
 // function doTheMath(numbers){
